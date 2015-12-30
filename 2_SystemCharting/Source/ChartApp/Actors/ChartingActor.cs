5e5596c766b598f87910e9e6202bc2755b23307f
Tania Shatilova <tatashat@gmail.com>
2015-12-30 18:14:50 +0300

    + scheduler

--- 2_SystemCharting/Source/ChartApp/Actors/ChartingActor.cs
+++ 2_SystemCharting/Source/ChartApp/Actors/ChartingActor.cs
@@ -8,6 +8,16 @@ namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
     {
+        /// <summary>
+        /// Maximum number of points we will allow in a series
+        /// </summary>
+        public const int MaxPoints = 250;
+
+        /// <summary>
+        /// Incrementing counter we use to plot along the X-axis
+        /// </summary>
+        private int xPosCounter = 0;
+
         #region Messages
 
         public class InitializeChart
@@ -33,6 +43,19 @@ namespace ChartApp.Actors
             public Series Series { get; set; }
         }
 
+        /// <summary>
+        /// Remove an existing <see cref="Series"/> from the chart
+        /// </summary>
+        public class RemoveSeries
+        {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
+        }
+
         #endregion
 
         private readonly Chart _chart;
@@ -49,9 +72,36 @@ namespace ChartApp.Actors
 
             Receive<InitializeChart>(ic => HandleInitialize(ic));
             Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
         }
         
         #region Individual Message Type Handlers
+
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) && _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints)
+                {
+                    series.Points.RemoveAt(0);
+                }
+                SetChartBoundaries();
+            }
+        }
 
         private void HandleAddSeries(AddSeries series)
         {
@@ -59,6 +109,7 @@ namespace ChartApp.Actors
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundaries();
             }
         }
 
@@ -73,6 +124,12 @@ namespace ChartApp.Actors
             //delete any existing series
             _chart.Series.Clear();
 
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
             //attempt to render the initial chart
             if (_seriesIndex.Any())
             {
@@ -82,6 +139,29 @@ namespace ChartApp.Actors
                     series.Value.Name = series.Key;
                     _chart.Series.Add(series.Value);
                 }
+            }
+
+            SetChartBoundaries();
+        }
+
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.Aggregate(new HashSet<DataPoint>(),
+                                                          (set, series) =>
+                                                          new HashSet<DataPoint>(set.Concat(series.Points)));
+            var yValues = allPoints.Aggregate(new List<double>(), (list, point) => list.Concat(point.YValues).ToList());
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
             }
         }
 
---
+++ 2_SystemCharting/Source/ChartApp/Actors/ChartingMessages.cs
@@ -0,0 +1,66 @@
+﻿using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    /// <summary>
+    ///     All types of counters supported by this example
+    /// </summary>
+    public enum CounterType
+    {
+        Cpu,
+        Memory,
+        Disk
+    }
+
+    /// <summary>
+    ///     Signal used to indicate that it's time to sample all counters
+    /// </summary>
+    public class GatherMetrics
+    {
+    }
+
+    /// <summary>
+    ///     Metric data at the time of sample
+    /// </summary>
+    public class Metric
+    {
+        public Metric(string series, float counterValue)
+        {
+            Series = series;
+            CounterValue = counterValue;
+        }
+
+        public float CounterValue { get; set; }
+        public string Series { get; set; }
+    }
+
+    /// <summary>
+    ///     Enables a counter and begins publishing values to <see cref="Subscriber" />.
+    /// </summary>
+    public class SubscribeCounter
+    {
+        public SubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Counter = counter;
+            Subscriber = subscriber;
+        }
+
+        public CounterType Counter { get; set; }
+        public IActorRef Subscriber { get; set; }
+    }
+
+    /// <summary>
+    ///     Unsubscribes <see cref="Subscriber" /> from receiving updates for a given counter
+    /// </summary>
+    public class UnsubscribeCounter
+    {
+        public UnsubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Counter = counter;
+            Subscriber = subscriber;
+        }
+
+        public CounterType Counter { get; set; }
+        public IActorRef Subscriber { get; set; }
+    }
+}
